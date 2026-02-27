d16fa2d3490c7a1222f1e1aea7b1a46603f8ed2e
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-02-27 17:02:18 +0100

    mis à jour pour local-Auth

--- android/app/src/main/kotlin/com/example/suivi_budget/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/suivi_budget/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.suivi_budget
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        // Register plugins
+        // GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+}
+
