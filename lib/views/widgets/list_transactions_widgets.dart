a7ac6381da7b5e8ef5d682eb73bfbfc7111a9e6d
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-03-05 20:55:29 +0100

    Affichage de la liste des transactions

--- lib/views/widgets/list_transactions_widgets.dart
+++ lib/views/widgets/list_transactions_widgets.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:skeletonizer/skeletonizer.dart';
+import 'package:provider/provider.dart';
+import 'package:suivi_budget/Providers/Database%20provider/database_provider.dart';
 import 'package:suivi_budget/views/widgets/transaction_card.dart';
 
 class ListTransactionsWidgets extends StatelessWidget {
@@ -7,28 +8,27 @@ class ListTransactionsWidgets extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Stream.empty(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Center(child: Text(snapshot.error.toString()));
-        }
+    return Consumer<DatabaseProvider>(
+      builder: (context, database, child) => StreamBuilder(
+        stream: database.transactions,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
 
-        if (snapshot.hasData && snapshot.data!.isEmpty ||
-            snapshot.data == null) {
-          return Center(child: Text("Aucun document trouvé"));
-        } else {
-          return Skeletonizer(
-            enabled: snapshot.connectionState == ConnectionState.waiting,
-            child: ListView.builder(
+          if (snapshot.hasData && snapshot.data!.isEmpty ||
+              snapshot.data == null) {
+            return Center(child: Text("Aucune transaction trouvé"));
+          } else {
+            return ListView.builder(
               itemCount: snapshot.data?.length,
               itemBuilder: (BuildContext context, int index) {
                 return TransactionCard(transaction: snapshot.data![index]);
               },
-            ),
-          );
-        }
-      },
+            );
+          }
+        },
+      ),
     );
   }
 }
