01356186722542b9c88135d474cafd88b1e56cb8
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-03-05 18:05:32 +0100

    modification de la classe modele

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,40 +1,20 @@
-class TransactionModel {
-  String id;
-  String userId;
-  int montant;
-  String category;
-  String description;
-  String type;
-  DateTime date;
-  TransactionModel({
-    required this.id,
-    required this.userId,
+import 'package:floor/floor.dart';
+
+@Entity(tableName: "transactions_table")
+class Transaction {
+  @primaryKey
+  final int? id;
+  final int montant;
+  final String category;
+  final String description;
+  final String type;
+  final String date;
+  Transaction({
+    this.id,
     required this.montant,
     required this.category,
     required this.description,
     required this.type,
     required this.date,
   });
-  factory TransactionModel.fromMap(Map<String, dynamic> map) {
-    return TransactionModel(
-      id: map['id'],
-      userId: map['userId'],
-      montant: map['montant'],
-      category: map['category'],
-      description: map['description'],
-      type: map['type'],
-      date: map['date'],
-    );
-  }
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'userId': userId,
-      'montant': montant,
-      'category': category,
-      'description': description,
-      'type': type,
-      'date': date,
-    };
-  }
 }
