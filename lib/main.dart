0d61dfd4bb29cb7a88695f1fe30475fe1d754327
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-03-07 00:35:34 +0100

    Suppression du provider 'Solde provider'

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:suivi_budget/Providers/Auth%20provider/auth_provider.dart';
 import 'package:suivi_budget/Providers/Database%20provider/database_provider.dart';
-import 'package:suivi_budget/Providers/Database%20provider/solde_provider.dart';
 import 'package:suivi_budget/Providers/modification_view_provider.dart/modifier_transaction_view_provider.dart';
 import 'package:suivi_budget/Services/database/transaction_database.dart';
 import 'package:suivi_budget/constants.dart';
@@ -17,13 +16,13 @@ void main() async {
     //Les providers
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => SoldeProvider()),
         ChangeNotifierProvider(
           create: (context) => ModifierTransactionViewProvider(),
         ),
         ChangeNotifierProvider(create: (context) => AuthProvider()),
         ChangeNotifierProvider(
-          create: (context) => DatabaseProvider(database: database),
+          create: (context) =>
+              DatabaseProvider(database: database)..listenTransactions(),
         ),
       ],
       child: MainApp(),
