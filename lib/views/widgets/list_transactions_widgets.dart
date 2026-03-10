c1fe390ce64396ef3b7764822f1968a719b01461
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-03-10 16:03:07 +0100

    Affichage des transactions en temps réel

--- lib/views/widgets/list_transactions_widgets.dart
+++ lib/views/widgets/list_transactions_widgets.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:skeletonizer/skeletonizer.dart';
+import 'package:suivi_budget/Providers/Database%20services%20provider/transaction_table_provider.dart';
 import 'package:suivi_budget/views/widgets/transaction_card.dart';
 
 class ListTransactionsWidgets extends StatelessWidget {
@@ -7,28 +9,30 @@ class ListTransactionsWidgets extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Stream.empty(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Center(child: Text(snapshot.error.toString()));
-        }
+    return Consumer<TransactionTableProvider>(
+      builder: (context, value, child) => StreamBuilder(
+        stream: value.transaction,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
 
-        if (snapshot.hasData && snapshot.data!.isEmpty ||
-            snapshot.data == null) {
-          return Center(child: Text("Aucun document trouvé"));
-        } else {
-          return Skeletonizer(
-            enabled: snapshot.connectionState == ConnectionState.waiting,
-            child: ListView.builder(
-              itemCount: snapshot.data?.length,
-              itemBuilder: (BuildContext context, int index) {
-                return TransactionCard(transaction: snapshot.data![index]);
-              },
-            ),
-          );
-        }
-      },
+          if (snapshot.hasData && snapshot.data!.isEmpty ||
+              snapshot.data == null) {
+            return Center(child: Text("Aucun document trouvé"));
+          } else {
+            return Skeletonizer(
+              enabled: snapshot.connectionState == ConnectionState.waiting,
+              child: ListView.builder(
+                itemCount: snapshot.data?.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return TransactionCard(transaction: snapshot.data![index]);
+                },
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
