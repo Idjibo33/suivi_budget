3861cbd0fd65059189f69ed89dde914105bb1d43
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-03-22 22:38:39 +0100

    Modification navigators

--- lib/models/navigation/naviguer_auth_gate.dart
+++ lib/models/navigation/naviguer_auth_gate.dart
@@ -1,21 +1,10 @@
-import 'dart:io';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:suivi_budget/auth_gate.dart';
 
 void naviguerAuthGate(BuildContext context) {
-  if (Platform.isIOS) {
-    Navigator.pushAndRemoveUntil(
-      context,
-      CupertinoDialogRoute(builder: (context) => AuthGate(), context: context),
-      (route) => false,
-    );
-  } else {
-    Navigator.pushAndRemoveUntil(
-      context,
-      MaterialPageRoute(builder: (context) => AuthGate()),
-      (route) => false,
-    );
-  }
+  Navigator.pushAndRemoveUntil(
+    context,
+    MaterialPageRoute(builder: (context) => AuthGate()),
+    (route) => false,
+  );
 }
