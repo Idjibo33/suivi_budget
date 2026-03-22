4705ed1a35a1e9b32292daf65a88c587536909d7
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-03-22 22:23:28 +0100

    Modification navigators

--- lib/Services/database/database.dart
+++ lib/Services/database/database.dart
@@ -1,26 +1,26 @@
 import 'package:suivi_budget/models/helpers/database.dart';
 
-class Database implements Databaseervices {
+class Database implements DatabaseServices {
   @override
-  Future<dynamic> createData() {
+  Future<dynamic> createData(item) {
     // TODO: implement createData
     throw UnimplementedError();
   }
 
   @override
-  Future<dynamic> deleteData() {
+  Future<dynamic> deleteData(String id) {
     // TODO: implement deleteData
     throw UnimplementedError();
   }
 
   @override
-  Future<dynamic> readData() {
+  Stream<dynamic> readData(String id) {
     // TODO: implement readData
     throw UnimplementedError();
   }
 
   @override
-  Future<dynamic> updateData() {
+  Future<dynamic> updateData(String id, item) {
     // TODO: implement updateData
     throw UnimplementedError();
   }
